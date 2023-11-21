6d1ff359fc57a94c93286f8dfa944a6270d082dc
NychyporukR <131202862+NychyporukR@users.noreply.github.com>
2023-11-21 19:58:20 +0200

    upd

---
+++ js/lesson1/js.js
@@ -0,0 +1,42 @@
+let a, b, c, d, e, f, g, h, i, j, k, l, m;
+a = 'hello';
+b = 'owu';
+c = 'com';
+d = 'ua';
+e = 1;
+f = 10;
+g = -999;
+h = 123;
+i = 3.14;
+j = 2.7;
+k = 16;
+l = true;
+m = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+let firstname, middlename, lastname, person;
+firstname = 'Roman ';
+middlename = 'Yuriyovych '
+lastname = 'Nychyporuk '
+person = lastname + firstname + middlename;
+console.log(person);
+
+let aa = 100;
+let bb = '100';
+let cc = true;
+console.log(typeof aa);
+console.log(typeof bb);
+console.log(typeof cc);
